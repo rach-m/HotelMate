44746fc507b8547715fa369b542708e71031283f
Rachel Moskowitz <rachelbmoskowitz@gmail.com>
2018-06-04 10:04:03 -0400

    updated connection file

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,5 @@
-const monitor = require("pg-monitor");
 const promise = require("bluebird");
+const monitor = require("pg-monitor");
 
 let initOptions = {};
 
@@ -19,10 +19,19 @@ monitor.attach(initOptions, ["query", "error"]);
 // Import pg-promise and initialize the library with an empty object.
 const pgp = require("pg-promise")(initOptions);
 
-// Prepare the connection URL from the format: 'postgres://username:password@host:port/database';
-const connectionURL = "postgres://localhost:5432/hotelmate_db";
+const databaseName = "hotelmate_db";
 
-// Creating a new database connection with the provided URL.
-const db = pgp(connectionURL);
+let connectionConfig;
+
+if (process.env.NODE_ENV === "development" || !process.env.NODE_ENV) {
+  // Prepare the connection URL from the format: 'postgres://username:password@host:port/database';
+  connectionConfig = `postgres://localhost:5432/${databaseName}`;
+} else if (process.env.NODE_ENV === "production") {
+  // Heroku will set this variable for you.
+  connectionConfig = process.env.DATABASE_URL;
+}
+
+// Creating a new database connection with the provided configuration.
+const db = pgp(connectionConfig);
 
 module.exports = db;
